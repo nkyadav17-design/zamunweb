846b3dd5237e03ee9d18202280f65aa2fc02cb64
Nawal Kishore <nawal@shrofile.com>
2025-12-22 12:43:41 +0530

    next config file update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,42 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['cdn.sanity.io'],
+    domains: ["cdn.sanity.io"],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/content-strategy-and-marketing",
+        destination: "/services/content-strategy-and-marketing",
+        permanent: true,
+      },
+      {
+        source: "/design-strategy-and-service",
+        destination: "/services/design-strategy-and-service",
+        permanent: true,
+      },
+      {
+        source: "/marketing-strategy-development",
+        destination: "/services/marketing-strategy-development",
+        permanent: true,
+      },
+      {
+        source: "/stl-enters-access-solutions-market",
+        destination: "/case-studies/stl-enters-access-solutions-market",
+        permanent: true,
+      },
+      {
+        source: "/zamun-report-powered-by-airtel",
+        destination: "/reports/wired-to-win",
+        permanent: true,
+      },
+      {
+        source: "/how-global-brands-shape-tech-standards",
+        destination: "/blogs/the-power-players-how-global-brands-shape-tech-standards",
+        permanent: true,
+      },
+    ];
   },
 };
 
