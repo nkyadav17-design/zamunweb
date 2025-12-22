70939bc3aa10991438424604750a2813a7d91526
Nawal Kishore <nawal@shrofile.com>
2025-12-22 18:21:09 +0530

    more next config file update

--- next.config.ts
+++ next.config.ts
@@ -36,6 +36,76 @@ const nextConfig = {
         destination: "/blogs/the-power-players-how-global-brands-shape-tech-standards",
         permanent: true,
       },
+      {
+        source: "/marketing-strategy-development-services",
+        destination: "/services/marketing-strategy-development",
+        permanent: true,
+      },
+      {
+        source: "/evs-green-mobility-or-greenwash",
+        destination: "/blogs/evs-green-mobility-or-greenwash",
+        permanent: true,
+      },
+      {
+        source: "/india-ev-revolution-public-transport-electric-transition",
+        destination: "/blogs/india-s-ev-revolution-how-is-public-transport-slowly-going-electric",
+        permanent: true,
+      },
+      {
+        source: "/building-a-thriving-ev-ecosystem-a-global-vision",
+        destination: "/blogs/building-a-thriving-ev-ecosystem-a-global-vision",
+        permanent: true,
+      },
+      {
+        source: "/ev-charging-a-major-departure-from-traditional-fueling",
+        destination: "/blogs/ev-charging-a-major-departure-from-traditional-fueling",
+        permanent: true,
+      },
+      {
+        source: "/space-data-a-new-commodity-transforming-life-on-earth",
+        destination: "/blogs/space-data-a-new-commodity-transforming-life-on-earth",
+        permanent: true,
+      },
+      {
+        source: "/weve-been-using-ai-for-longer-than-we-thought",
+        destination: "/blogs/we-ve-been-using-ai-for-longer-than-we-thought",
+        permanent: true,
+      },
+      {
+        source: "/ev-battery-recycling-a-key-to-sustainability-and-affordability",
+        destination: "/blogs/ev-battery-recycling-a-key-to-sustainability-and-affordability",
+        permanent: true,
+      },
+      {
+        source: "/quantum-calling-unlocking-the-power-of-quantum-tech-in-telecommunications",
+        destination: "/blogs/quantum-calling-unlocking-the-power-of-quantum-tech-in-telecommunications",
+        permanent: true,
+      },
+      {
+        source: "/quantum-dreams-vs-reality-where-we-are-and-whats-next",
+        destination: "/blogs/quantum-dreams-vs-reality-where-we-are-and-what-s-next",
+        permanent: true,
+      },
+      {
+        source: "/unlocking-growth-why-smart-companies-invest-heavily-in-strategic-marketing",
+        destination: "/blogs/unlocking-growth-why-smart-companies-invest-heavily-in-strategic-marketing",
+        permanent: true,
+      },
+      {
+        source: "/next-gen-bevs-a-sustainable-future-but-with-a-repair-catch",
+        destination: "/blogs/next-gen-bevs-a-sustainable-future-but-with-a-repair-catch",
+        permanent: true,
+      },
+      {
+        source: "/next-gen-bevs-a-sustainable-future-but-with-a-repair-catch",
+        destination: "/blogs/next-gen-bevs-a-sustainable-future-but-with-a-repair-catch",
+        permanent: true,
+      },
+      {
+        source: "/from-follower-to-leader-indias-rise-in-shaping-global-industrial-standards",
+        destination: "/blogs/from-follower-to-leader-india-s-rise-in-shaping-global-industrial-standards",
+        permanent: true,
+      },
     ];
   },
 };
